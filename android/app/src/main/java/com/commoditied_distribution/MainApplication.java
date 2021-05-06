7b51ead588ce562b767ec26711a8826f9b18e1e8
Ajay Pandit <ajayrpandit2529.ap@gmail.com>
2021-05-06 22:13:18 +0530

    removed unwanted dependencies

--- android/app/src/main/java/com/commoditied_distribution/MainApplication.java
+++ android/app/src/main/java/com/commoditied_distribution/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
-import com.upi.payment.UpiPaymentPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
